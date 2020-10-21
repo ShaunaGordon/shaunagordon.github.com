87ead8d0f3f0f012896a9f56958d4f28c4e12c4c
Shauna Gordon <shauna@shaunagordon.com>
2020-10-21 23:50:01 +0000

    Deploy shaunagordon.com to github.com/ShaunaGordon/shaunagordon.github.com.git:master

---
+++ js/site.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var flip = function () {
+      var target = $('.pusher')
+      if (bodyBgSwitchIndex === 0) {
+        bodyBgSwitchIndex = 1
+        target.toggleClass('flip-it')
+        setTimeout( function () {
+          $('.front').css('display','none')
+          $('.back').css('display','block')
+        }, 300)
+      }
+        else {
+          bodyBgSwitchIndex = 0
+          target.toggleClass('flip-it')
+          setTimeout( function (){
+            $('.front').css('display','block')
+            $('.back').css('display','none')
+        }, 300)
+      }
+  };
+
+  $(".ui.accordion").accordion();
+  $("#tag-category-pop").click(function() {
+    $(".hand.point.icon").toggleClass("active");
+  });
+
+  $('.dream-flip-toggle').click(flip);
+
+  var backHeadings = [
+    '#about-me',
+    '#hiring-me',
+    '#buy-me-coffee'
+  ];
+
+  if(backHeadings.includes(window.location.hash)) {
+    flip();
+  }
+});
